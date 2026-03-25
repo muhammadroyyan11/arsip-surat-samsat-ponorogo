721d767c4d0a99510941334ff11ca71986724150
unknown <royyanmz87@gmail.com>
2026-03-26 03:27:42 +0700

    fix: explicit table name for Disposisi to fix pluralization error

--- app/Models/Disposisi.php
+++ app/Models/Disposisi.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Disposisi extends Model
 {
     use HasFactory;
+    protected $table = 'disposis';
     protected $guarded = [];
 
     public function suratMasuk()
